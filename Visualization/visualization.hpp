3cb0ba63196c2855bf8453a5bd7239a910db286d
p02lonad <p02lonad@uco.es>
2023-11-11 11:51:34 +0100

    Doxygen update

--- Visualization/visualization.hpp
+++ Visualization/visualization.hpp
@@ -3,13 +3,29 @@
 
 #include "infoAcquisition.hpp"
 
-
+/**
+ * @brief Displays the process information
+ * 
+ * @param processes The processes of the system
+ * @param number_of_processes The number of processes displayed
+ * @param arguments How the processes are going to be displayed
+ */
 void DisplayProcessInfo(std::vector<ProcessInfo>& processes, unsigned number_of_processes,
     std::vector<int> arguments);
 
+/**
+ * @brief Displays CPU usage
+ * 
+ * @param process The process whose information will be displayed
+ */
 void displayCPUUsage(struct ProcessInfo process);
 
-void displayCNetUsage(struct ProcessInfo process);
+/**
+ * @brief Displays net usage
+ * 
+ * @param process The process whose information will be displayed
+ */
+void displayNetUsage(struct ProcessInfo process);
 
 /** Function to get key without waiting
  * @param timeoutMs timeout in milliseconds
